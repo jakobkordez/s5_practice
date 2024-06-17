5c97b5d4024602c1ad8d18a1a0344c7b88f7cb51
Jakob Kordež <jakobkordez1999@gmail.com>
2024-06-17 21:27:37 +0200

    Migrate pwa to Serwist

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-});
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require("next/constants");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+    /** @type {import("next").NextConfig} */
+    const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+        const withSerwist = (await import("@serwist/next")).default({
+            // Note: This is only an example. If you use Pages Router,
+            // use something else that works, such as "service-worker/index.ts".
+            swSrc: "src/app/sw.ts",
+            swDest: "public/sw.js",
+        });
+        return withSerwist(nextConfig);
+    }
+
+    return nextConfig;
+};
