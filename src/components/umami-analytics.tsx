f9ed03166c981e14a31d0cb228c098bda36b829a
Jakob Kordež <jakobkordez1999@gmail.com>
2025-01-11 17:31:02 +0100

    Fix any

--- src/components/umami-analytics.tsx
+++ src/components/umami-analytics.tsx
@@ -5,7 +5,10 @@ import Script from 'next/script';
 const ANALYTICS_URL = process.env.NEXT_PUBLIC_ANALYTICS_URL;
 const ANALYTICS_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
-export function umamiTrack(eventName: string, data?: { [key: string]: any }) {
+export function umamiTrack(
+  eventName: string,
+  data?: { [key: string]: string | number },
+) {
   if (typeof window === 'undefined') return;
 
   const umami = (
